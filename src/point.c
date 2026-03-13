a0bee2bbdb7c0127eedd2aeff6b262404fead47c
Moshe Sherman <mtshrmn@gmail.com>
2026-03-14 00:32:39 +0200

    remove more unused code

--- src/point.c
+++ src/point.c
@@ -50,45 +50,6 @@ double point_length(Point p) {
   return point_length;
 }
 
-double point_dist2(Point a, Point b) {
-  double dx = b.x - a.x;
-  double dy = b.y - a.y;
-  return dx * dx + dy * dy;
-}
-
-double point_dist2_to_segment(Point p, Point a, Point b) {
-  double len2 = point_dist2(a, b);
-  if (len2 == 0) {
-    return point_dist2(p, a);
-  }
-
-  double dx = b.x - a.x;
-  double dy = b.y - a.y;
-  // projection parameter - dot product of (p - a) and (b - a)
-  double t = ((p.x - a.x) * dx + (p.y - a.y) * dy) / len2;
-  // clamp it to be on the segment
-  t = t < 0 ? 0 : (t > 1 ? 1 : t);
-  Point closest = {
-      .x = a.x + t * dx,
-      .y = a.y + t * dy,
-  };
-  return point_dist2(closest, p);
-}
-
-// B(t) = (1 - t)^3 * p0 + 3(1-t)^2t * p1 + 3(1-t)t^2 * p2 + t^3 * p3
-Point point_bezier_at(double t, Point p0, Point p1, Point p2, Point p3) {
-  double u = 1.0 - t;
-  double u2 = u * u;
-  double u3 = u2 * u;
-  double t2 = t * t;
-  double t3 = t2 * t;
-  Point result = {
-      .x = u3 * p0.x + 3 * u2 * t * p1.x + 3 * u * t2 * p2.x + t3 * p3.x,
-      .y = u3 * p0.y + 3 * u2 * t * p1.y + 3 * u * t2 * p2.y + t3 * p3.y,
-  };
-  return result;
-}
-
 void create_handle_triple(Point *p1, Point *p2, Point *p3, Point *h1, Point *h2) {
   Point parallel_direction_next = point_normalize(point_subtruct(*p3, *p1));
   Point delta = point_subtruct(*p2, *p1);
